2ebdf97e6372f068d62c0e89d03d265aec95e2a3
aofleejay <aofleejay@hotmail.com>
2019-03-29 18:27:30 +0700

    :sparkles: Connect to Okta (OAuth2 Provider) with AppAuth.

--- App.js
+++ App.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import SafariView from "react-native-safari-view"
-import { AUTHORIZATION_URL, CLIENT_ID, REDIRECT_URL } from 'react-native-dotenv'
+import { ScrollView, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { ISSUER, CLIENT_ID, REDIRECT_URL } from 'react-native-dotenv'
+import { authorize, refresh, revoke } from 'react-native-app-auth'
+
+const config = {
+  issuer: ISSUER,
+  clientId: CLIENT_ID,
+  redirectUrl: REDIRECT_URL,
+  scopes: ['openid', 'profile', 'offline_access']
+}
 
 class App extends Component {
-  _openBrowser = (url) => {
-    SafariView.isAvailable()
-      .then(SafariView.show({ url }))
-      .catch(error => { console.error(error) })
+  state = { auth: null }
+
+  _authorize = async () => {
+    try {
+      const authState = await authorize(config)
+      this.setState({ auth: authState })
+    } catch (error) {
+      console.log(error)
+    }    
   }
 
-  _openAuthorizePage = () => {
-    this._openBrowser(`${AUTHORIZATION_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}`)
+  _refreshToken = async () => {
+    try {
+      const refreshState = await refresh(config, { refreshToken: this.state.auth.refreshToken })
+      this.setState({ auth: refreshState })
+    } catch (error) {
+      console.log(error)
+    }    
+  }
+
+  _revokeToken = async () => {
+    try {
+      await revoke(config, { tokenToRevoke: this.state.auth.accessToken, sendClientId: true })
+      this.setState({ auth: null })
+    } catch (error) {
+      console.log(error)
+    }    
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this._openAuthorizePage}>
+      <ScrollView style={styles.container}>
+        <Text>{JSON.stringify(this.state.auth)}</Text>
+        <TouchableOpacity onPress={this._authorize}>
           <Text>Authorize</Text>
         </TouchableOpacity>
-      </View>
+        <TouchableOpacity onPress={this._refreshToken}>
+          <Text>Refresh Token</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this._revokeToken}>
+          <Text>Revoke Token</Text>
+        </TouchableOpacity>
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 20,
   },
 })
 
